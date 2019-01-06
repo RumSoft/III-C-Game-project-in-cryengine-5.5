7bff6604d4232d8cf96414e853375964af7cfbaf
Dominik Ruman <dr124@o2.pl>
2019-01-06 20:15:29 +0100

    x

--- Code/Components/Actor/Actor.h
+++ Code/Components/Actor/Actor.h
@@ -3,7 +3,19 @@
 #include "GamePlugin.h"
 #include <DefaultComponents/Geometry/AdvancedAnimationComponent.h>
 #include <DefaultComponents/Physics/CharacterControllerComponent.h>
+#include <DefaultComponents/AI/PathfindingComponent.h>
+#include <CryAISystem/Components/IEntityNavigationComponent.h>
+#include <CryAISystem/Components/IEntityCoverUserComponent.h>
+#include <CryAISystem/Components/IEntityBehaviorTreeComponent.h>
+#include <CryAISystem/Components/IEntityObservableComponent.h>
+//#include <CryAISystem/Components/IEntityObserverComponent.h>
+#include <CryAISystem/Components/IEntityListenerComponent.h>
+#include <CryAISystem/Components/IEntityFactionComponent.h>
+//#include <DefaultComponents/Effects/ParticleComponent.h>
 #include "State/IActorState.h"
+#include <DefaultComponents/Input/InputComponent.h>
+
+class IEntityObserverComponent;
 
 class CActor : public IActor
 {
@@ -27,20 +39,65 @@ public:
 	void OnSpawn() override {};
 
 	void Revive() override;
+
+	void AttackActor(CActor* enemy)
+	{
+		m_pPathfindingComponent->RequestMoveTo(enemy->GetEntity()->GetWorldPos());
+	}
+	void MoveTo(Vec3 pos)
+	{
+		Logger::Get().Log(GetEntity()->GetName(), "moves somewhere");
+	}
+	void AttackMelee(CActor* enemy) {}
+	void Hide() {  }
+	bool CanSeeEnemy()
+	{
+		
+		return nullptr;
+	}
+	int DealDamage(int damage) { Health -= damage; if (Health <= 0) Kill(); }
+
+
+	void Kill() { actorState = EActorState::Killed;}
+
+	enum EActorState
+	{
+		NO_STATE, Idle, Alarmed,Attacking, Fleeing, Killed, END
+	};
+
+	EActorState actorState;
+
+	std::vector<CActor*> enemyQueue;
+
+	int Faction;
+
+	int Health;
+	int MaxHealth;
+
 protected:
+	void UpdateAI(float fFrameTime);
 	void Update(float fFrameTime) override;
 	IActorState* GetState() { return m_pActorState; }
 private:
 	Cry::DefaultComponents::CCharacterControllerComponent* m_pCharacterController = nullptr;
 	Cry::DefaultComponents::CAdvancedAnimationComponent* m_pAnimationComponent = nullptr;
 	Cry::DefaultComponents::CInputComponent* m_pInputComponent = nullptr;
-
+	Cry::DefaultComponents::CPathfindingComponent* m_pPathfindingComponent = nullptr;
+	IEntityCoverUserComponent* m_pCoverUserComponent = nullptr;
+	IEntityNavigationComponent* m_pNavigationComponent = nullptr;
+	IEntityListenerComponent* m_pListenerComponent = nullptr;
+	IEntityObservableComponent* m_pObservableComponent = nullptr;
+	IEntityObserverComponent* m_pObserverComponent = nullptr;
+	IEntityBehaviorTreeComponent* m_pBehaviorTreeComponent = nullptr;
+	IEntityFactionComponent* m_pFactionComponent = nullptr;
 	IActorState* m_pActorState = nullptr;
 
 	FragmentID m_idleFragmentId;
 	FragmentID m_walkFragmentId;
 	FragmentID m_activeFragmentId;
 	TagID m_rotateTagId;
+	float m_movementSpeed = 10;
 
+	float m_aiupdatetime = 1;
 };
 
